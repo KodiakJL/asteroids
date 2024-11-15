b8b9c8895362088357643c2182a4409a6a119ace
KodiakJL <jacobol14@gmail.com>
2024-11-15 13:47:50 -0600

    We now have asteroids that apear, no collision yet

--- main.py
+++ main.py
@@ -4,13 +4,27 @@
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroidfield = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    as_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -18,8 +32,13 @@ def main():
                 return
             
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for u in updatable:
+            u.update(dt)
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
 
         #limit the framerate to 60 FPS
